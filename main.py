584b0e0bf8362c9c77be296821267f0d68ec44f4
m1nk1 <m1nk1@naver.com>
2025-11-26 22:18:58 +0900

    Chat UI

--- main.py
+++ main.py
@@ -1,49 +1,69 @@
+from altair.vegalite.v5.schema.core import Text
+import dotenv
+import asyncio
 import streamlit as st
-import time
+from agents import Agent, Runner, SQLiteSession
+from streamlit import delta_generator
 
-st.header("Hello world!")
+dotenv.load_dotenv()
 
-st.button("Click me please!")
+# agent 설정 (웹페이지가 리로드 되도 1번만 실행되도록 설정)
+if "agent" not in st.session_state:
+  st.session_state["agent"] = Agent(
+    name="ChatGPT Clone",
+    instructions="""
+    You are a helpful assistant.
+    """,
+  )
 
-st.text_input(
-    "Write your API KEY",
-    max_chars=20,
-)
+agent = st.session_state["agent"]
 
-st.feedback("faces")
+
+# memory 설정 (웹페이지가 리로드 되도 1번만 실행되도록 설정)
+if "session" not in st.session_state:
+  st.session_state["session"] = SQLiteSession(
+    "chat-history",
+    "chat-gpt-clone-memory.db",
+  )
+
+session = st.session_state["session"]
+
+def to_generator(delta):
+  text = delta if isinstance(delta, str) else str(delta)
+
+  for ch in text:
+    yield ch
+
+async def run_agent(message):
+  stream = Runner.run_streamed(
+    agent,
+    message,
+    session=session,
+  )
+
+  async for event in stream.stream_events():
+    if event.type == "raw_response_event":
+      if event.data.type == "response.output_text.delta":
+        with st.chat_message("ai"):
+          st.write_stream(to_generator(event.data.delta))
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+  with st.chat_message("human"):
+    st.write(prompt)
+
+  asyncio.run(run_agent(prompt))
+
+
 with st.sidebar:
-    st.badge("Badge 1")
+  reset = st.button("Reset memory")
 
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+  if reset:
+    asyncio.run(session.clear_session())
+  
+  st.write(asyncio.run(session.get_items()))
 
-with tab1:
-    st.header("Agent")
-with tab2:
-    st.header("Agent 2")
-with tab3:
-    st.header("Agent 3")
-
-with st.chat_message("ai"):
-    st.text("Hello!")
-
-    with st.status("Agent is using tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the web...")
-
-        time.sleep(2)
-        status.update(label="Agnet is reading the page...")
-        
-        time.sleep(3)
-        status.update(state="complete")
-
-    with st.chat_message("human"):
-        st.text("Hi!")
-
-   
-    st.chat_input(
-        "stWrite a messa for the assistant.",
-        accept_file  = True,
-    )
 
 # 실행방법
 # uv run streamlit run main.py
---
+++ stream_test.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import time
+
+st.header("Hello world!")
+
+st.button("Click me please!")
+
+st.text_input(
+    "Write your API KEY",
+    max_chars=20,
+)
+
+st.feedback("faces")
+with st.sidebar:
+    st.badge("Badge 1")
+
+tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+
+with tab1:
+    st.header("Agent")
+with tab2:
+    st.header("Agent 2")
+with tab3:
+    st.header("Agent 3")
+
+with st.chat_message("ai"):
+    st.text("Hello!")
+
+    with st.status("Agent is using tool") as status:
+        time.sleep(1)
+        status.update(label="Agent is searching the web...")
+
+        time.sleep(2)
+        status.update(label="Agnet is reading the page...")
+        
+        time.sleep(3)
+        status.update(state="complete")
+
+    with st.chat_message("human"):
+        st.text("Hi!")
+
+   
+    st.chat_input(
+        "stWrite a messa for the assistant.",
+        accept_file  = True,
+    )
+
+# 실행방법
+# uv run streamlit run main.py
